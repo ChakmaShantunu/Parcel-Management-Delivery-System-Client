75ba225c48c072af71a37fe23987d82fcb6cbc70
shantunu007 <chakmashantunu.web@gmail.com>
2025-08-27 12:33:21 +0600

    navbar and footer section done

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../pages/shared/Navbar/Navbar';
+import Footer from '../pages/shared/Footer/Footer';
 
 const RootLayout = () => {
     return (
@@ -9,6 +10,9 @@ const RootLayout = () => {
                 <Navbar></Navbar>
             </nav>
             <Outlet></Outlet>
+            <footer>
+                <Footer></Footer>
+            </footer>
         </div>
     );
 };
