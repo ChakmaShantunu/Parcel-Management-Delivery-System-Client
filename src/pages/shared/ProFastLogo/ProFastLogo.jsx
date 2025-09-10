c9732e3ffdafcc0b1fc07840d5a53a40e24c9e15
shantunu007 <chakmashantunu.web@gmail.com>
2025-09-10 09:07:25 +0600

    link wrapped on logo

--- src/pages/shared/ProFastLogo/ProFastLogo.jsx
+++ src/pages/shared/ProFastLogo/ProFastLogo.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from '../../../assets/logo.png'
+import { Link } from 'react-router';
 
 const ProFastLogo = () => {
     return (
-        <div className='flex items-end'>
-            <img className='mb-2' src={logo} alt="ProFast company logo" />
-            <h1 className='font-extrabold text-5xl ml-2'>ProFast</h1>
-        </div>
+        <Link to="/">
+            <div className='flex items-end'>
+                <img className='mb-2' src={logo} alt="ProFast company logo" />
+                <h1 className='font-extrabold text-5xl ml-2'>ProFast</h1>
+            </div>
+        </Link>
     );
 };
 
